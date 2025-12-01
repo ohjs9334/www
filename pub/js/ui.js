bdf05fb513008a586e43daa852e92d8aaa405ad3
jungwoo oh <ohjs9334@gmail.com>
2025-12-02 01:42:21 +0900

    리스트, 헤더 :: 작업중

---
+++ pub/js/ui.js
@@ -0,0 +1,10 @@
+$(function(){
+    // gnb hover시
+    $(".header_area").mouseover(function(){
+        $(this).addClass("hover")
+    });
+	$(".header_area").mouseout(function(){
+        $(this).removeClass("hover")
+    });
+
+})
