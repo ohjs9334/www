3e377266101ac93ece44e26fc1c5d766adf1f90e
woou <hxx_ara@naver.com>
2025-12-12 03:31:00 +0900

    feat: modal_close 오류 해결 - ui.js에 초기 작성된 modal_close 소스와 company에 있는 소스가 충돌한 내용 - ui.js에 초기에 작성한 소스 제거 후 테스팅 > 오류 해결

--- pub/js/ui.js
+++ pub/js/ui.js
@@ -95,14 +95,6 @@ $(function(){
 
     });
 
-
-    //modal
-    // modal_practice 내 닫기 버튼 클릭 시 모달 닫기
-    $(document).on("click", ".modal_practice .modal_close", function () {
-        $(".modal_overlay").hide();
-        $(".modal_practice").removeClass("open").hide();
-    });
-
     // inner tab list 터치무브
     $('.inner_tab_list').on('touchmove', function (e) {
         e.stopPropagation();
